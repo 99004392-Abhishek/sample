6bf6235434f6a74e810437d13655fc63bd9ef15e
99004392-Abhishek <abhishek.kunwar@ltts.com>
2021-06-06 20:02:46 +0530

    initial commit 5

--- yes/example.py
+++ yes/example.py
@@ -1,1 +1,23 @@
-print("hello world")
+from openpyxl import workbook, load_workbook
+
+wb = load_workbook('Genesis mini_project.xlsx')
+
+ws = wb.active
+
+def my_ps_number(ip_ws):
+    ps_nums=[]
+    for row in ws.iter_rows(min_row=1, max_col=1, max_row=16, values_only=True):
+        ps_nums.append(list(row))
+    return ps_nums
+
+def show_ps_number(ps):
+    for item in ps:
+        print(item)
+
+def main():
+    print("\nEnter the PS number from the below list:\n")
+    ps = my_ps_number(ws)
+    show_ps_number(ps)
+    user_choice = int(input("\nEnter the Ps number:"))
+
+main()
